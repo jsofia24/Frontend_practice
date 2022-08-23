1aaddd8c3727da93080c531e67132462c1d478ef
jsofia24 <jsofia24@hotmail.com>
2022-08-23 15:31:38 -0500

    capturando valores

---
+++ prueba.js
@@ -0,0 +1,6 @@
+function funcion1(){
+    let x=prompt("Digite #1: ");
+    let y=prompt("Digite #2: ");
+    let z=parseInt(x)+parseInt(y);
+    document.getElementById("prueba").innerHTML="El valor de z es: "+z;
+}
