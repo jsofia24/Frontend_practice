68e19f3227e77d17c041053b5c95a99de08ce147
jsofia24 <jsofia24@hotmail.com>
2022-08-29 15:32:50 -0500

    Bootstrap

---
+++ login.js
@@ -0,0 +1,11 @@
+const user= "sofia"
+const pw= 1234
+function verificar(){
+    let usuario = document.getElementById("nombre").value
+    let clave= document.getElementById("clave").value
+    if(user == usuario && pw == clave){
+        alert("Login exitoso")
+    }else{
+        alert("Verifique los datos ingresados")
+    }
+}
